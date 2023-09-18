f5188f61a0193395f6ef6f5a8a0849b786b479f2
Carlos Napoles Avila <napolesmoa1006@gmail.com>
2023-09-18 16:57:26 -0400

    Initial config

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
 require('dotenv').config()
 
+const express = require('express')
+const app = express()
+
+const mongoose = require("mongoose") // <-- Import the mongoose module
+const cors = require('cors')
+
+
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
 
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Now connected to MongoDB!')
+  })
+  .catch((err) => {
+    console.error('Something went wrong', err)
+  })
 
-
+  
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
+  console.log('Your app is listening on http://localhost:' + listener.address().port)
 })
